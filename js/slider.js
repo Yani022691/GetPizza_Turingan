bdd8b26d056daa539b2b8dc97d1ad3b33e7782af
Khrizie <2303476@ub.edu.ph>
2026-03-06 09:50:32 +0800

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+const slider = tns({
+  container: ".testimonialSlider",
+  items: 1,
+  slideBy: "page",
+  speed: 100,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  rewind: true,
+  controls: false,
+  nav: false,
+});
